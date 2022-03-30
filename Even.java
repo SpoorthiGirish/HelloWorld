7dacda01ac5f3c31be554403c3d5d47d048f982e
Spoorthi G <spog@publicisgroupe.net>
2022-03-30 14:16:20 +0530

    evenfile

---
+++ Even.java
@@ -0,0 +1,11 @@
+public class Even
+{
+public static void main(String[] args)
+{
+   int num=12;
+   if(num%2==0)
+    System.out.println("even number");
+	else
+  System.out.println("odd number");	
+}
+}
